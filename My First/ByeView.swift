6f2f3fa485ffc2095deaf2c2f5576948b7715163
Humphrey Hanson <humphreyhanson@icloud.com>
2023-05-04 15:54:40 +0200

    First initial try

---
+++ My First/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  My First
+//
+//  Created by Humphrey Hanson on 04.05.23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("HAVE YOU MET ME!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
